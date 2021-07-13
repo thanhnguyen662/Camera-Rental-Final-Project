3e4fe3501a88b01f25c78d0026240433ccdcb6c0
Thanh Nguyen <facebook662311@gmail.com>
2021-07-13 23:01:20 +0700

    90% product detail page :heavy_check_mark:

--- client/src/features/Product/components/ProductDetail/index.jsx
+++ client/src/features/Product/components/ProductDetail/index.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 // import BreadcrumbBar from '../../../../components/BreadcrumbBar';
 import './ProductDetail.scss';
-import { Row, Col, Button, DatePicker } from 'antd';
+import { Row, Col, Button, DatePicker, Image, Avatar, Divider } from 'antd';
+import { BiCheckShield, BiBulb, BiCloudSnow } from 'react-icons/bi';
 import userApi from '../../../../api/userApi';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { useSelector } from 'react-redux';
+import conversationApi from '../../../../api/conversationApi';
 
 ProductDetail.propTypes = {
    productDetail: PropTypes.object,
@@ -20,6 +23,7 @@ const { RangePicker } = DatePicker;
 function ProductDetail(props) {
    const { productDetail } = props;
    const [userProductInfo, setUserProductInfo] = useState('');
+   const userId = useSelector((state) => state.users.id);
 
    function onChange(dates, dateStrings) {
       if (!dates || !dateStrings) return;
@@ -41,16 +45,34 @@ function ProductDetail(props) {
       getInfoByFirebaseId();
    }, [productDetail?.firebaseId]);
 
+   const onClickSendMessage = async () => {
+      try {
+         if (!userId || !productDetail) return console.log('WAIT FOR REDUX');
+         const formValues = {
+            senderId: userId,
+            receiverId: productDetail?.firebaseId,
+         };
+         const response = await conversationApi.createConversation(formValues);
+         console.log('Conversation created: ', response);
+
+         localStorage.setItem('selectedConversation', JSON.stringify(response));
+         window.location = '/message';
+      } catch (error) {
+         return console.log('Error: ', error);
+      }
+   };
+
    return (
       <>
          {/* <BreadcrumbBar productName={productDetail} /> */}
          <div className='productDetailCard'>
             <Row span={24} gutter={[45, 0]}>
-               <Col span={11} className='rowImage'>
+               <Col span={12} className='rowImage'>
                   <Row span={24}>
                      <Col span={24}>
                         <div className='mainImage'>
-                           <img
+                           <Image
+                              className='image'
                               src={productDetail.productPhotoURL}
                               alt='photos'
                            />
@@ -71,42 +93,67 @@ function ProductDetail(props) {
                      </Col>
                   </Row>
                </Col>
-               <Col span={13} className='test'>
-                  <h1>{productDetail.name}</h1>
+               <Col span={12} className='product'>
                   <h2>
                      <p>BY</p>
-                     <Link to={`/profile/${productDetail.firebaseId}`}>
-                        {userProductInfo.email}
-                     </Link>
+                     {userProductInfo.email}
                   </h2>
+                  <h1>{productDetail.name}</h1>
+                  <h5>The One and Only</h5>
                   <div className='productDetail'>
-                     <Row>
-                        <Col span={5} style={{ textAlign: 'center' }}>
-                           <h2>${productDetail.price}</h2>
+                     <h2>${productDetail.price}</h2>
+                     <h6>
+                        Sit nisi ipsum qui veniam culpa exercitation laborum.
+                     </h6>
+                  </div>
+                  <div className='productDescriptionInfo'>
+                     <Row className='descriptionInfoRow'>
+                        <Col flex='20px'>
+                           <BiCheckShield
+                              style={{ fontSize: '20px', color: '#7e8996' }}
+                           />
                         </Col>
-
-                        <Col span={19}>
-                           <h3>
-                              Save 7% <p>Duis esse amet magna cupidatat</p>
-                           </h3>
+                        <Col>
+                           <h4>Elit commodo duis.</h4>
+                        </Col>
+                     </Row>
+                     <Row className='descriptionInfoRow'>
+                        <Col flex='20px'>
+                           <BiBulb
+                              style={{ fontSize: '20px', color: '#7e8996' }}
+                           />
+                        </Col>
+                        <Col>
+                           <h4>Cillum et officia tempor.</h4>
+                        </Col>
+                     </Row>
+                     <Row className='descriptionInfoRow'>
+                        <Col flex='20px'>
+                           <BiCloudSnow
+                              style={{ fontSize: '20px', color: '#7e8996' }}
+                           />
+                        </Col>
+                        <Col>
+                           <h4>Qui nisi commodo do.</h4>
                         </Col>
                      </Row>
                   </div>
-                  <h3>{productDetail.description}</h3>
-                  <Button className='addButton'>Add to Cart</Button>
-                  <RangePicker
-                     className='datePicker'
-                     ranges={{
-                        Today: [moment(), moment()],
-                        'This Month': [
-                           moment().startOf('month'),
-                           moment().endOf('month'),
-                        ],
-                     }}
-                     showTime
-                     format='YYYY/MM/DD HH:mm'
-                     onChange={onChange}
-                  />
+                  <Row span={24} className='buttonRow'>
+                     <RangePicker
+                        className='datePicker'
+                        ranges={{
+                           Today: [moment(), moment()],
+                           'This Month': [
+                              moment().startOf('month'),
+                              moment().endOf('month'),
+                           ],
+                        }}
+                        showTime
+                        format='YYYY/MM/DD HH:mm'
+                        onChange={onChange}
+                     />
+                     <Button className='addButton'>Add to Cart</Button>
+                  </Row>
                </Col>
             </Row>
          </div>
